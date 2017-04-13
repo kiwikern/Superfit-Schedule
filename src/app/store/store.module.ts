03ee2e460816ac475ce9c9a9ed7b64df346b2002
Kim Kern <kim.kern@fu-berlin.de>
2017-04-13 20:36:30 +0200

    add localstorage support

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -13,14 +13,23 @@ import { rootReducer } from './root.reducers';
   providers: [RootActions, RootEpics]
 })
 export class StoreModule {
-  constructor(
-    public store: NgRedux<IAppState>,
-    rootEpics: RootEpics,
-    devTools: DevToolsExtension
-  ) {
+  constructor(public store: NgRedux<IAppState>,
+              rootEpics: RootEpics,
+              devTools: DevToolsExtension) {
+
+    const LOCAL_STORAGE_KEY = 'sfs.state';
+    let persistedState;
+    try {
+      persistedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    } catch(e) {
+      persistedState = {};
+    }
     store.configureStore(rootReducer,
-      {},
+      persistedState,
       [...rootEpics.createEpics()],
-    devTools.isEnabled() ? [devTools.enhancer()] : []);
+      devTools.isEnabled() ? [devTools.enhancer()] : []);
+    store.subscribe(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store.getState()))
+    })
   }
 }
