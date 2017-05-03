301b5a7c70b6320cdf34c1a37dcf1872b5d22abf
Kim Kern <kim.kern@fu-berlin.de>
2017-05-04 01:20:55 +0200

    class list component: set default days layout according to screen size, option can override

--- src/app/fitness-class/store/settings.reducers.ts
+++ src/app/fitness-class/store/settings.reducers.ts
@@ -8,7 +8,6 @@ export const INITIAL_STATE: ISettingsState = {
   showDaysInClasses: true,
   showSingleStudio: true,
   showTodayFirst: true,
-  daysLayout: 'column'
 };
 
 export function settingsReducer(state: ISettingsState = INITIAL_STATE,
@@ -30,7 +29,7 @@ interface ISettingsState {
   showDaysInClasses: boolean;
   showSingleStudio: boolean;
   showTodayFirst: boolean;
-  daysLayout: string;
+  daysLayout?: string;
 }
 
 export interface SettingsPayload {
