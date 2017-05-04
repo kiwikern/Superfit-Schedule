1be58a307ea1560638cbc7dc5b30b644b846b64b
Kim Kern <kim.kern@fu-berlin.de>
2017-05-04 22:34:05 +0200

    performance: classes will be only filtered once with multiple filters

--- src/app/fitness-class/pipes/filter-classes.pipe.ts
+++ src/app/fitness-class/pipes/filter-classes.pipe.ts
@@ -4,38 +4,23 @@ import { FilterState } from '../interfaces/filter-state';
 
 @Pipe({
   name: 'filterClasses',
-  pure: false
 })
 export class FilterClassesPipe implements PipeTransform {
 
   transform(classes: FitnessClass[], filterState?: FilterState): FitnessClass[] {
     if (!filterState) {
       return classes;
+    } else {
+      return classes.filter(c =>
+        (!filterState.days || filterState.days.includes(c.day)) &&
+        (!filterState.gyms || filterState.gyms.includes(c.gym)) &&
+        (!filterState.workouts || filterState.workouts.includes(c.workoutId)) &&
+        (!filterState.durations || filterState.durations.includes(c.duration)) &&
+        (!filterState.languages || filterState.languages.includes(c.language)) &&
+        (!filterState.minStartTime || filterState.minStartTime >= c.startTime) &&
+        (!filterState.maxStartTime || filterState.maxStartTime >= c.startTime)
+      );
     }
-    let filteredClasses = classes;
-    if (filterState.days) {
-      filteredClasses = filteredClasses.filter(c => filterState.days.includes(c.day));
-    }
-    if (filterState.gyms) {
-      filteredClasses = filteredClasses.filter(c => filterState.gyms.includes(c.gym));
-    }
-    if (filterState.workouts) {
-      filteredClasses = filteredClasses.filter(c => filterState.workouts.includes(c.workoutId));
-    }
-    if (filterState.durations) {
-      filteredClasses = filteredClasses.filter(c => filterState.durations.includes(c.duration));
-    }
-    if (filterState.languages) {
-      filteredClasses = filteredClasses.filter(c => filterState.languages.includes(c.language));
-    }
-    if (filterState.minStartTime) {
-      filteredClasses = filteredClasses.filter(c => filterState.minStartTime <= c.startTime);
-    }
-    if (filterState.maxStartTime) {
-      filteredClasses = filteredClasses.filter(c => filterState.maxStartTime >= c.startTime);
-    }
-
-    return filteredClasses;
   }
 
 }
