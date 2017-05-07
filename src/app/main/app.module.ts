19f532c856239da7bc2312f0e66ab964a09f0bb6
Kim Kern <kim.kern@fu-berlin.de>
2017-05-07 23:16:13 +0200

    move app module and component to own folders

--- src/app/main/app.module.ts
+++ src/app/main/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { NgReduxModule } from '@angular-redux/store';
-import { StoreModule } from './store/store.module';
-import { FitnessScheduleModule } from './fitness-schedule/fitness-schedule.module';
+import { StoreModule } from '../store/store.module';
+import { FitnessScheduleModule } from '../fitness-schedule/fitness-schedule.module';
 import 'hammerjs';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { SfsMaterialModule } from './material/sfs-material.module';
+import { AboutComponent } from '../about/about.component';
+import { SfsMaterialModule } from '../material/sfs-material.module';
 
 
 const appRoutes: Routes = [
--- src/app/main/app/app.component.spec.ts
+++ src/app/main/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { async, TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { Component } from '@angular/core';
-import { SfsMaterialModule } from './material/sfs-material.module';
+import { SfsMaterialModule } from '../../material/sfs-material.module';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
