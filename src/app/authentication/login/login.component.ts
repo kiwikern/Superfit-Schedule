13c6b9e652cbf6fa1332e8addbce24d157a11ec1
Kim Kern <github@kimkern.de>
2017-06-02 00:24:56 +0200

    new(LoginComponent): add login form and route

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userName: string;
+  password: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // TODO: login
   }
 
 }
