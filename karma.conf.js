80d7bd6dc3ca32cebdc86d0b53175a0c0a4c2652
Kim Kern <github@kimkern.de>
2017-06-02 18:56:21 +0200

    fix(UnitTests): include hammerjs and theme for MaterialModule

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,13 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './src/test.ts', watched: false }
+      { pattern: './src/test.ts', watched: false },
+
+      // Include a Material theme in the test suite.
+      { pattern: './node_modules/@angular/material/prebuilt-themes/indigo-pink.css', included: true, watched: false },
+
+      // Include hammerjs for Material.
+      { pattern: './node_modules/hammerjs/hammer.min.js', included: true, watched: false }
     ],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
