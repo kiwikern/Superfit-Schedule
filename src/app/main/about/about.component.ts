a41c782f453b4ca1b2b52e95e3cf8b02db2630db
Kim Kern <github@kimkern.de>
2017-06-07 00:57:03 +0200

    new(AboutComponent): add preliminary legal text

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   mailAddress = 'sfs@kimkern.de';
+  showLegal = false;
   constructor() { }
 
   ngOnInit() {
