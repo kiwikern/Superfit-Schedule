753c33c7b3c04673df780252aee0fbbdf6e8db0b
Kim Kern <github@kimkern.de>
2017-06-24 01:40:13 +0200

    fix(ScheduleFilterPipe): fix endTime filter

--- src/app/fitness-schedule/pipes/filter-classes.pipe.ts
+++ src/app/fitness-schedule/pipes/filter-classes.pipe.ts
@@ -44,7 +44,7 @@ export class FilterClassesPipe implements PipeTransform {
       const startMinutes = workout.startMinute;
       const duration = workout.duration;
       const endHour = moment().hour(workout.startHour).minutes(startMinutes + duration).hour();
-      return (maxEndTime > endHour) || (maxEndTime === endHour && startMinutes === 0);
+      return (maxEndTime > endHour) || (maxEndTime === endHour && (startMinutes + duration) % 60 === 0);
     }
   }
 
