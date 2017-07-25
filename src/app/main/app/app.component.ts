ea749dc29c18cb24ca2d0ef37908cdee99112307
--global <github@kimkern.de>
2017-07-25 17:01:35 +0200

    change(AppShell): add back button in toolbar

--- src/app/main/app/app.component.ts
+++ src/app/main/app/app.component.ts
@@ -11,7 +11,9 @@ import { Angulartics2, Angulartics2GoogleAnalytics, Angulartics2Piwik } from 'an
 })
 export class AppComponent implements OnInit {
 
+  private isChild = false;
   @select(['authentication', 'userName']) userName$: Observable<string>;
+  @select() router$: Observable<string>;
 
   constructor(private swUpdateNotifications: SwUpdateNotificationsService,
               private angulartics: Angulartics2,
@@ -22,5 +24,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.swUpdateNotifications.enable();
     this.userName$.subscribe(userName => this.angulartics.setUsername.next(userName));
+    this.router$.subscribe(path => this.isChild = this.isChildPath(path));
+  }
+
+  private isChildPath(path: string): boolean {
+    return /(feedback\/.*)/.test(path);
   }
 }
