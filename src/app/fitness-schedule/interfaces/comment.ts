fd1025cce4439fa5643ec3a86867d07f9520b6c8
--global <github@kimkern.de>
2017-07-25 18:24:56 +0200

    new(Comments): add comment interface

---
+++ src/app/fitness-schedule/interfaces/comment.ts
@@ -0,0 +1,10 @@
+export interface Comment {
+  classId: string;
+  userId: string;
+  text: string;
+  instructor: string;
+  attendance: number;
+  hasGoodTechnique: boolean;
+  hasGoodEncouragement: boolean;
+  hasGoodMusic: boolean;
+}
