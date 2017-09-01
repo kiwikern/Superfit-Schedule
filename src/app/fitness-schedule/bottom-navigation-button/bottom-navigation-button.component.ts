9f9f5ac84d050be18e346453d4404fdacc1c4bec
--global <github@kimkern.de>
2017-09-01 10:07:15 +0200

    fix(BottomNav): remove params and anchors for url matching

--- src/app/fitness-schedule/bottom-navigation-button/bottom-navigation-button.component.ts
+++ src/app/fitness-schedule/bottom-navigation-button/bottom-navigation-button.component.ts
@@ -21,12 +21,28 @@ export class BottomNavigationButtonComponent implements OnInit {
   }
 
   isActive(): boolean {
+    const activePath = this.getActivePath();
     if (this.scheduleRoute !== '/') {
       const fullRoute = '/schedule' + this.scheduleRoute;
-      return this.activeRoute === fullRoute;
+      return activePath === fullRoute;
     } else {
-      return this.activeRoute === '/schedule';
+      return activePath === '/schedule';
     }
   }
 
+  private getActivePath() {
+    let activePath = this.activeRoute;
+
+    const pathIndex = this.activeRoute.indexOf('?');
+    if (pathIndex !== -1) {
+      activePath = activePath.substr(0, pathIndex);
+    }
+
+    const anchorIndex = this.activeRoute.indexOf('#');
+    if (anchorIndex !== -1) {
+      activePath = activePath.substr(0, anchorIndex);
+    }
+
+    return activePath;
+  }
 }
