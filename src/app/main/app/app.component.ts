478a93887d0f5e8c458fd37223d18c13a689d6e3
--global <github@kimkern.de>
2017-11-10 13:35:11 +0100

    change(Navigation): make all non-schedule routes children and when clicking on the backwards arrow route back to last schedule route.

--- src/app/main/app/app.component.ts
+++ src/app/main/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
   isChild = false;
   parentPath: string;
   fragment: string;
+  lastRoute: string;
   @select(['authentication', 'userName']) userName$: Observable<string>;
   @select() router$: Observable<string>;
   @select(['authentication', 'jwt']) jwt$;
@@ -31,7 +32,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.swUpdatesService.updateActivated.subscribe();
     this.userName$.subscribe(userName => this.angulartics.setUsername.next(userName));
-    this.router$.subscribe(path => this.isChild = this.isChildPath(path));
+    this.router$.subscribe(path => this.setChildPath(path));
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.scroll(this.router.url);
@@ -39,23 +40,27 @@ export class AppComponent implements OnInit {
     });
   }
 
-  private isChildPath(path: string): boolean {
+  private setChildPath(path: string) {
+    this.fragment = null;
     if (/(feedback\/.*)/.test(path)) {
       this.parentPath = '/feedback';
-      this.fragment = null;
-      return true;
+      this.isChild = true;
     } else if (/(schedule\/class.*\/new)/.test(path)) {
       this.parentPath = path.substring(0, path.length - 4);
-      this.fragment = null;
-      return true;
+      this.isChild = true;
     } else if (/(schedule\/class.*)/.test(path)) {
       this.parentPath = '/schedule';
       this.fragment = this.getClassId(path);
-      return true;
-    } else {
+      this.isChild = true;
+    } else if (/(schedule\.*)/.test(path)) {
       this.parentPath = null;
-      this.fragment = null;
-      return false;
+      this.isChild = false;
+    } else {
+      this.parentPath = this.lastRoute ? this.lastRoute : '/';
+      this.isChild = true;
+    }
+    if (/(schedule\.*)/.test(path)) {
+      this.lastRoute = path;
     }
   }
 
