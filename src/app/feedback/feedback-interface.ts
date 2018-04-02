4652f0d0ac8200402620488dbab5e47759c3f431
--global <github@kimkern.de>
2018-04-02 19:34:39 +0200

    change(feedback): add userId to feedback

--- src/app/feedback/feedback-interface.ts
+++ src/app/feedback/feedback-interface.ts
@@ -1,10 +1,11 @@
-import { FeedbackResponse } from './response-interface';
+import {FeedbackResponse} from './response-interface';
 
 export interface Feedback {
   id: string;
   text: string;
   date: Date;
   responses: FeedbackResponse[];
+  userId?: string;
   satisfaction?: number;
   device?: string;
   os?: string;
